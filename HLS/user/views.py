cd20f8faa4e5a448b6796d9e64e84200d8b72f36
GeonWooPaeng <gwpaeng@naver.com>
2020-09-23 17:51:49 +0900

    로그인

--- HLS/user/views.py
+++ HLS/user/views.py
@@ -1,31 +1,22 @@
+from django.http import HttpResponse 
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from .models import User 
+from .forms import LoginForm
 # Create your views here.
 
 
 
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-
-    elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-
-        res_data = {}
-        if not (username and password):
-            res_data['error'] = '값을 모두 입력하세요'
-        else:
-            user = User.objects.get(username=username)
-            if check_password(password, user.password):
-                request.session['user'] = user.id 
-                return redirect('/')
-            else:
-                res_data['error'] = '비밀번호가 다릅니다.'
-
-        return render(request, 'login.html', res_data)
-            
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid(): 
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm() 
+    
+    return render(request, 'login.html', {'form': form})
 
 
 def register(request): #회원가입
