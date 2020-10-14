ae0b07bde07a0c62dd1f2813a7827e6557b0496a
GeonWooPaeng <gwpaeng@naver.com>
2020-10-14 17:12:55 +0900

    timer

--- HLS/timer/views.py
+++ HLS/timer/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+# from django.views.generic import ListView
+# from .models import Timer
 # Create your views here.
 
 def timer(request):
     return render(request, 'timer.html')
+
+
+# class TimerRank(ListView):
+#     model = Timer
+#     template_name = 'timer_rank.html'
+#     context_object_name = 'timer_list'
