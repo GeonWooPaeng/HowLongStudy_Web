e5d1ab48c492e7e4895760850e217e507ff405d2
GeonWooPaeng <gwpaeng@naver.com>
2020-11-12 17:06:27 +0900

    1st Done

--- HLS/timer/views.py
+++ HLS/timer/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
 from django.utils.decorators import method_decorator
-from user.decorators import login_required
 from django.views.generic import ListView,FormView 
+from django.core.paginator import Paginator
+from user.decorators import login_required
 from user.models import User
 from .models import Timer
-
-
 
 
 def SaveTime(request):
@@ -35,7 +34,21 @@ def SaveTime(request):
     return render(request, 'timer.html')
 
 
+def board_list(request):
+    boards_all = Timer.objects.all().order_by('-id')
+    page = int(request.GET.get('p',1))
+    paginator = Paginator(boards_all, 5) 
+    boards = paginator.get_page(page)
+    return render(request, "timer_rank.html",{"boards":boards})
+
+
 class TimerList(ListView):
-    model = Timer
     template_name = 'timer_rank.html'
-    context_object_name= 'timer_list'
+    context_object_name= 'timers'
+    
+    def get_queryset(self):
+        timer_all = Timer.objects.all().order_by('-id')
+        page = int(self.request.GET.get('p',1))
+        paginator = Paginator(timer_all,5)
+        queryset = paginator.get_page(page)
+        return queryset
